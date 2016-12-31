51a7cf7c5b4c82b6c14a29d37fad29c435ba20cb
jonfreedman <jon.freedman@zoho.com>
2016-12-31 12:41:59 +0000

    export variables

--- it/run-odp.sh
+++ it/run-odp.sh
@@ -6,11 +6,11 @@
 # $ brew upgrade node
 # $ npm install
 
-SYMPHONY_HOST=foundation-dev.symphony.com
-SYMPHONY_KM_HOST=foundation-dev-api.symphony.com
-SYMPHONY_SESSIONAUTH_HOST=foundation-dev-api.symphony.com
-SYMPHONY_AGENT_HOST=foundation-dev-api.symphony.com
+export SYMPHONY_HOST=foundation-dev.symphony.com
+export SYMPHONY_KM_HOST=foundation-dev-api.symphony.com
+export SYMPHONY_SESSIONAUTH_HOST=foundation-dev-api.symphony.com
+export SYMPHONY_AGENT_HOST=foundation-dev-api.symphony.com
 
-HUBOT_SYMPHONY_LOG_LEVEL=notice
+export HUBOT_SYMPHONY_LOG_LEVEL=notice
 
 npm run it
